99aaa23c131c6a76fe8e5de5dcc17e466df07474
raghul2000 <100715186+raghul2000@users.noreply.github.com>
2022-10-21 12:56:51 -0500

    Add files via upload

---
+++ botresponse.js
@@ -0,0 +1,11 @@
+﻿function getBotResponse(input) {
+
+    // test responses
+    if (input == "hello") {
+        return "How can I help ?";
+    } else if (input == "bye") {
+        return "happy to help!";
+    } else {
+        return "Try asking something else!";
+    }
+}
